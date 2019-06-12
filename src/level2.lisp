439cbf70d01e48abc83456deb223e39175b1eeee
fgatherlet <fgatherlet@gmail.com>
2019-06-12 20:18:17 +0900

    adding level2 utils.

--- src/level2.lisp
+++ src/level2.lisp
@@ -27,9 +27,7 @@
         (setf (wd-window) (car diff))
       (error "can not create window"))))
 
-
-(defun wd-enable-tooltip ()
-  (wd-execute "
+(defparameter inject-util "
 function xpath(xpathq) {
   var headings = document.evaluate(xpathq, document, null, XPathResult.ANY_TYPE, null );
   var node = headings.iterateNext();
@@ -40,19 +38,60 @@ function xpath(xpathq) {
   }
   return result;
 }
+")
+(defparameter inject-disable-tooltip "
+xpath('//div[@xtype=\\'xtooltip\\']').forEach((elm)=>{
+  elm.parentElement.removeChild(elm);
+});
+")
+
+(defun wd-disable-tooltip ()
+  (wd-execute (concatenate
+               'string
+               inject-util
+               inject-disable-tooltip
+               )))
+
+(defun wd-enable-tooltip ()
+  (wd-execute 
+   (concatenate
+    'string
+    inject-util
+    inject-disable-tooltip
+    "
 var elms = xpath('//a');
 var i = 0;
 elms.forEach(elm=>{
   elm.setAttribute('xid', `${i}`);
-  elm.textContent = `${i}-${elm.textContent}`;
+  //elm.textContent = `${i}-${elm.textContent}`;
+  var tt = document.createElement('div');
+  tt.style['display'] = 'block';
+  tt.style['position'] = 'absolute';
+  tt.style['bottom'] = '-2.8em';
+  tt.style['left'] = '0.3em';
+  tt.style['z-index'] = '9999';
+  tt.style['width'] =  'auto';
+  tt.style['height'] =  'auto';
+  tt.style['padding'] =  '0.3em 0.5em';
+  tt.style['color'] =  '#FFFFFF';
+  tt.style['background'] =  '#c72439';
+  tt.style['border-radius'] =  '0.5em';
+  tt.style[''] = '';
+  tt.style[''] = '';
+  tt.style[''] = '';
+  tt.setAttribute('xtype', 'xtooltip');
+  tt.innerHTML = `${i}`;
+  elm.appendChild(tt);
   i = i + 1;
 });
+
 
 function xref(id) {
   var elm = xpath(`//a[@xid = ${id}]`)[0];
   return elm;
 }
-"))
+")))
+
 (defun wd-click-tooltip (i)
   (wd-element-click (wd-find (format nil "//a[@xid = ~a]" i))))
 
@@ -60,3 +99,31 @@ function xref(id) {
   (wd-actions (:keys :page-up)))
 (defun wd-page-down ()
   (wd-actions (:keys :page-down)))
+
+
+#|
+https://unguis.cre8or.jp/web/1934
+.sample5-tooltips {
+	display: block;
+	position: absolute;
+	bottom: -2.8em;
+	left: 0.3em;
+	z-index: 9999;
+	width: auto;
+	height: auto;
+	padding: 0.3em 0.5em;
+	color: #FFFFFF;
+	background: #c72439;
+	border-radius: 0.5em;
+}
+.sample5-tooltips:after {
+	width: 100%;
+	content: "";
+	display: block;
+	position: absolute;
+	left: 0.5em;
+	top: -8px;
+	border-top:8px solid transparent;
+	border-left:8px solid #c72439;
+}
+|#
